eaa89365d4ca4116958d385655b25389c749a43a
ItsMeSugan <t.suganeeshwaran@gmail.com>
2025-10-28 23:58:28 +0530

    initial commit

---
+++ src/test/java/com/sugan/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugan.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
