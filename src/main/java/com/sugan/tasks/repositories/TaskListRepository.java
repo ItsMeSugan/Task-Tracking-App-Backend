dba90d12ebbc4858cb09167bf5a6ceb029b21e2d
ItsMeSugan <t.suganeeshwaran@gmail.com>
2025-10-29 15:08:48 +0530

    2 repository interfaces created for Task and TaskList classes

---
+++ src/main/java/com/sugan/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,9 @@
+package com.sugan.tasks.repositories;
+
+import com.sugan.tasks.doman.entities.TaskList;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {}
---
+++ src/main/java/com/sugan/tasks/repositories/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.sugan.tasks.repositories;
+
+import com.sugan.tasks.doman.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+  List<Task> findByTaskListId(UUID taskListID);
+
+  Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+}
